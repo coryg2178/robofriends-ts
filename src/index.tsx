23581d2a3fd2c15e66ff7d8de0cdac34c9b0715d
coryg <coryg2178@gmail.com>
2023-04-23 22:03:48 -0400

    feat: part 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
-import Hello from './Hello';
 import 'tachyons';
+import { robots } from './robots';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Hello greeting={'Hello' + 'React Ninjas'} />
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+    </div>
   </React.StrictMode>
 );
 
